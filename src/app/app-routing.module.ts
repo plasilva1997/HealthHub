427b4a031ef75dd0e7d87d5693514f51fff82def
Yann Poher <yan.poher@gmail.com>
2021-05-06 11:49:14 +0200

    save gender

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'gender',
+    loadChildren: () => import('./pages/gender/gender.module').then(m => m.GenderPageModule)
+  },
+
 ];
 
 @NgModule({
