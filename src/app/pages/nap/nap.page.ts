efb142a9e0d264973c83cac4b40224eaa01e0649
Yann Poher <yan.poher@gmail.com>
2021-06-29 12:04:02 +0200

    page nap

--- src/app/pages/nap/nap.page.ts
+++ src/app/pages/nap/nap.page.ts
@@ -14,7 +14,17 @@ export class NapPage implements OnInit {
   }
 
   toastActionSedentaire() {
-    this.toastService.presentToastNap("Activité physique faible, vous travailler devant un ordinateur ou êtes assis\n" +
-      "toute la journée, vous vous déplacer peu à pied et ne pratiquez aucun sport")
+    this.toastService.presentToastNap("Activité physique faible, vous travailler devant un ordinateur ou êtes assis " +
+      "toute la journée, vous vous déplacer peu à pied et ne pratiquez aucun sport.")
+  }
+  toastActionActif() {
+    this.toastService.presentToastNap("Activité modéré, vous vous déplacez régulièrement à pied et vous pratiquez " +
+    "toutes sortes d’activités, votre travail n’est pas éprouvant physiquement, vous êtes régulièrement " +
+    "debout, vous ne pratiquez pas un sport régulièrement.")
+  }
+  toastActionSportif() {
+    this.toastService.presentToastNap("Activité sportif élevée, vous vous déplacez régulièrement à pied et vous pratiquez toutes " +
+    "sortes d'activités, vous êtes régulièrement debout, votre travail est éprouvant physiquement " +
+    "ou vous pratiquez un sport régulièrement.")
   }
 }
