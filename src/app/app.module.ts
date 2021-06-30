6aec242a829b1fc92c56d8cc299868e082b1e351
plasilva1997 <72378847+plasilva1997@users.noreply.github.com>
2021-06-30 10:11:56 +0200

    test api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from "@angular/common/http";
-import { NativeStorage } from "@ionic-native/native-storage";
+import { NativeStorage } from "@ionic-native/native-storage/ngx";
 
 @NgModule({
   declarations: [AppComponent],
